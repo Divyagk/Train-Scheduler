618606f1e0478b3d91d5fcab9d9d55c8c341501e
divya <gopalakrishnandivya6@gmail.com>
2018-11-25 12:23:57 -0700

    commit4

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,5 @@
 $(document).ready(function(){
     var config = {
-        apiKey: "AIzaSyBPCAxjycmuYaeY8RISjpPzUOSfmZIbQrQ",
-        authDomain: "train-scheduler-68643.firebaseapp.com",
-        databaseURL: "https://train-scheduler-68643.firebaseio.com",
-        projectId: "train-scheduler-68643",
-        storageBucket: "train-scheduler-68643.appspot.com",
-        messagingSenderId: "934455543386"
-    };
-    firebase.initializeApp(config);
-    
-
-<script>
-  // Initialize Firebase
-  var config = {
     apiKey: "AIzaSyDuxFJKGiqu5_42_q03l09hftlwgdPIN40",
     authDomain: "bootcamp-cdeef.firebaseapp.com",
     databaseURL: "https://bootcamp-cdeef.firebaseio.com",
@@ -21,7 +8,31 @@ $(document).ready(function(){
     messagingSenderId: "1056053592800"
   };
   firebase.initializeApp(config);
-</script>
+  
+var database = firebase.database();
+var name;
+var destination;
+var firstTrain;
+var frequency = 0;
+
+$("#add-train-btn").on("click", function() {
+    event.preventDefault();
+    // Storing and retreiving new train data
+    name = $("#train-name").val().trim();
+    destination = $("#destination").val().trim();
+    firstTrain = $("#first-train").val().trim();
+    frequency = $("#frequency").val().trim();
+
+    // Pushing to database
+    database.ref().push({
+        name: name,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+    $("form")[0].reset();
+});
 
 
 
