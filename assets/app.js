4d2273e1908089c0e79af90bfc6142163a53e623
divya <gopalakrishnandivya6@gmail.com>
2018-11-25 12:14:00 -0700

    commit3

---
+++ assets/app.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    var config = {
+        apiKey: "AIzaSyBPCAxjycmuYaeY8RISjpPzUOSfmZIbQrQ",
+        authDomain: "train-scheduler-68643.firebaseapp.com",
+        databaseURL: "https://train-scheduler-68643.firebaseio.com",
+        projectId: "train-scheduler-68643",
+        storageBucket: "train-scheduler-68643.appspot.com",
+        messagingSenderId: "934455543386"
+    };
+    firebase.initializeApp(config);
+    
+
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDuxFJKGiqu5_42_q03l09hftlwgdPIN40",
+    authDomain: "bootcamp-cdeef.firebaseapp.com",
+    databaseURL: "https://bootcamp-cdeef.firebaseio.com",
+    projectId: "bootcamp-cdeef",
+    storageBucket: "bootcamp-cdeef.appspot.com",
+    messagingSenderId: "1056053592800"
+  };
+  firebase.initializeApp(config);
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
